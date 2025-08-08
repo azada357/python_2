b72a93593388fc9d2c09e0234d2734c120537a19
Azada Madadi <azadamadadi@gmail.com>
2025-08-08 13:24:31 +0430

    hello world

---
+++ Aza.py
@@ -0,0 +1,10 @@
+thisList = ["apple", "banana", "cherry"]
+
+thisList[0] = "blackcurrent"
+
+print(thisList)
+
+thisList = ["apple", "banana", "cherry", "waterelon", "pear", "orange"]
+
+del thisList[2]
+print(thisList)W
